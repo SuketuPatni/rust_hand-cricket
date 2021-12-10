fad61039329136e211b991b7cf68f0922f096357
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-10 17:56:44 +0530

    random integer and boolean and input enhanced

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use rand::Rng;
-use std::collections::HashMap;
+mod user;
+mod system;
+// mod tie;
 
 fn input_num(prompt:&str) -> u32 {
     let mut str = String::new();
@@ -16,27 +18,39 @@ fn input(prompt:&str) -> String {
     String::from(str.trim())
 }
 
-const ODDEVE_TUP:[&str;2] = ["odd","even"];
+const ODDEVE_TUP:[&str;2] = ["even", "odd"];
 
 fn random() -> u32 {
     rand::thread_rng().gen_range(0..7)
 }
 
+fn random_bool() -> bool {
+    rand::thread_rng().gen_bool(0.5)
+}
+
 fn main() {
-    // let mut a = rand::thread_rng().gen_range(0..7);
-    // let a = String::new();
-    let a = input("Choose heads or tails: ");
-    println!("{}", a);
-    let toss = rand::thread_rng().gen_bool(0.5);
-    if toss {
+    let _ = input("Choose heads or tails: ");
+
+    let sys_oe:u32 = random();
+
+    if random_bool() {
         // user wins toss
         // odd or eve
         let odd_eve = input("Choose odd or eve: ");
-        let sys_oe:u32 = random();
         let user_oe = input_num("Enter your odd-eve throw: ");
         let parity = (user_oe + sys_oe)%2;
         if ODDEVE_TUP[parity as usize] == odd_eve {
-
+            // user wins odd eve draw
+            let choice = input("You won the odd-eve draw.\nChoose to bat or bowl: ");
+            if choice == "bat" {
+                user::batting();
+            } else {
+                system::batting();
+            }
         }
+    } else {
+        // user loses toss
+        // odd or eve
+        
     }
 }
